5868f6e6cc6d9b6b007b977d2acc648bab92f644
MT-Blachetta <86985738+MT-Blachetta@users.noreply.github.com>
2023-01-04 20:23:57 +0100

    alpha release

--- full_app.cpp
+++ full_app.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 string savepath;
 
-
 unsigned int num_wohnung;
 vector<string> wohnung; // name
 vector<float> quadratmeter_wohnung; // qm
@@ -125,7 +124,6 @@ void input_value(float& number, const std::string& message) {
 
 }
 
-
 void print_general_values() {
 	
     printf("\nWASSER\n");
@@ -193,6 +191,138 @@ void kennzahl_abfrage() {
 
     input_value(abwasserkosten, "Geben Sie die Gesamtkosten fuer Abwasser in Euro an (Beispiel 39.99): ");
     cout << "Abwasserkosten in Euro = " << abwasserkosten << "\n";
+
+
+}
+
+void wohnung_aendern(unsigned int i) {
+
+    if (i >= num_wohnung) { 
+        cout << "FEHLER: Ungueltige Eingabe. Die eingegebene Wohnungs-ID gehoehrt zu keiner existierenden Wohnung; Aktion abgebrochen" << endl;
+        return;
+    }
+
+    char opt;
+    cout << "Welcher Wert von Wohnung " << i << " von " << wohnung[i] << " soll geaendert werden ?" << endl;
+    cout << "a: Name/Haushalt" << endl;
+    cout << "b: Flaeche in m2 " << endl;
+    cout << "c: Anzahl Personen" << endl;
+    cout << "d: Stand des Wasserzaehlers" << endl;
+    cout << "e: Stand des Stromzaehlers" << endl;
+    cout << "f: Heizungen" << endl;
+    cout << "g: ABBRUCH" << endl;
+    cout << "\nGeben Sie den richtigen Buchstaben fuer den jeweiligen Wert an, den Sie aendern moechten: ";
+    cin >> opt;
+
+    switch(opt){
+
+        case 'a':
+            string wohnungsname;
+            char confirm = 'y';
+            do {
+                 cout << "Geben Sie den Namen der Wohnung ein: ";
+                 cin >> wohnungsname;
+                 cout << "Eingabe: " << wohnungsname << " | bestaetigen 'y', aendern 'n': ";
+                 cin >> confirm;
+                 cout << endl;
+               } while (confirm == 'n');
+            wohnung[i] = wohnungsname;
+            break;
+
+        case 'b':
+            input_value(quadratmeter_wohnung[i], "Geben Sie die Flaeche der Wohnung in m2 an: ");
+            cout << endl;
+            break;
+
+        case 'c':
+            bool check = true;
+            string person_str;
+            unsigned int personen;
+            while (check) {
+            try {
+                    char confirm = 'y';
+                    do {
+                        cout << "Wie viele Personen wohnen im Raum ? (einfache Zahl): ";
+                        cin >> person_str;
+                        personen = std::stoul(person_str);
+                        cout << "Eingabe: " << personen << " | bestaetigen 'y', aendern 'n': ";
+                        cin >> confirm;
+                        cout << endl;
+                       } while (confirm == 'n');
+                    check = false;
+                }
+            catch (const std::invalid_argument& e) {
+            std::cout << "Die Eingegebene Zeichenfolge ist keine gueltige Zahl ! Sie muessen den Wert nochmal korrekt angeben:\n";
+            continue;
+                                                   }
+                           }
+            personen_wohnung[i] = personen;
+            break;
+
+        case 'd': // Stand des Wasserzaehlers
+            input_value(wasserzaehler[i], "Geben Sie den Stand des Wasserzaehlers in m3 ein: ");
+            cout << endl;
+            break;
+        case 'e': // Stromzaehler
+            input_value(stromzaehler[i], "Geben Sie den Stand des Stromzaehlers in kW/h ein: ");
+            cout << endl;        
+            break;
+        case 'f':
+            float heizung = 0;
+            cout << "Legen sie die Raeume der Wohnung neu an und geben den Zaehlerstand fuer jede Heizung der Wohnung ein.\n";
+            char next_room = 'n';
+
+            do {
+                string raumname;
+                cout << "Raum (Name): ";
+                cin >> raumname;
+
+                cout << "Geben Sie den Stand der ersten (oder einzigen) Heizung an, weitere Heizungen koennen hinzugefuegt werden";
+                float tmp;
+                float hz = 0;
+                input_value(tmp, "\nZaehlerstand der Heizung: ");
+                hz += tmp;
+                cout << "Heizung (Gesamteinheiten) von " << raumname << ": " << hz << endl;
+
+                char next = 'n';
+                cout << "Weiteren Heizung Zaehler angeben ? (y/n): ";
+                cin >> next;
+
+                while (next == 'y') {
+                    input_value(tmp, "\nZaehlerstand der Heizung: ");
+                    hz += tmp;
+                    cout << "Heizung (Gesamteinheiten) von " << raumname << ": " << hz << endl;
+
+                    cout << "Weitere Heizung angeben ? (y/n): ";
+                    cin >> next;
+                                    }
+
+                heizung += hz;
+                cout << "\nNaechsten Raum hinzufuegen ? (y/n): ";
+                cin >> next_room;
+            } while (next_room == 'y');
+            heizungszaehler[i] = heizung;
+            break;
+
+        case 'g':
+            cout << "Aktion abgebrochen" << endl;
+            return;
+        default:
+            cout << "FEHLER: Ungueltige Option, Aktion wird abgebrochen" << endl;
+            return;
+
+
+    }
+
+
+    cout << "Wohnung wurde geaendert -->" << endl;
+    cout << "Name: " << wohnung[i] << endl;
+    cout << "Flaeche: " << quadratmeter_wohnung[i] << endl;
+    cout << "Bewohnerzahl: " << personen_wohnung[i] << endl;
+    cout << "Strom: " << stromzaehler[i] << endl;
+    cout << "Wasser: " << wasserzaehler[i] << endl;
+    cout << "Heizung(Gas): " << heizungszaehler[i] << endl;
+
 
 
 }
@@ -429,6 +559,8 @@ void load_main_data(string& dataline) {
 
     }
 
+
+
 void load_home_data(string& dataline) {
 
     //add an apartment
@@ -476,8 +608,27 @@ void load_home_data(string& dataline) {
 
 }
 
-void change_single_value(char val_ID){
+void change_single_value() {
     
+  char val_ID;
+
+  cout << "Welche Zahl moechten sie aendern ?" << endl;
+  cout << "a: Wasser Gesamtkosten in Euro" << endl;
+  cout << "b: Wasser Gesamtverbrauch in m3" << endl;
+  cout << "c: Gas Gesatkosten in Euro" << endl;
+  cout << "d: Gas Gesamtverbrauch in m3" << endl;
+  cout << "e: Strompreis pro kW/h in Euro" << endl;
+  cout << "f: allgemeiner Stromverbrauch in kW/h" << endl;
+  cout << "g: Grundsteuer in Euro" << endl;
+  cout << "h: Muellabfuhrkosten in Euro" << endl;
+  cout << "i: Wohngebaeudeversicherung (gesamt) in Euro" << endl;
+  cout << "j: Strassenreinigung in Euro" << endl;
+  cout << "k: Abwasserkosten in Euro" << endl;
+  cout << "l: ABBRECHEN" << endl;
+  cout << "\nGeben Sie den richtigen Buchstaben fuer die jeweilige Kennzahl an, die Sie aendern moechten: ";
+  cin >> val_ID;
+  
+
   switch (val_ID) {
     case 'a':
         input_value(wasser_gesamtkosten, "Wie lauten die Gesamtkosten fuer den Wasserverbrauch des gesamten Hauses in Euro (Beispiel: 189.99): ");
@@ -524,16 +675,21 @@ void change_single_value(char val_ID){
         cout << "Abwasserkosten in Euro = " << abwasserkosten << "\n";
         return;
     case 'l':
-	return;		  
+	    return;		  
     default:
-	cout << "Ungueltige Eingabe --> Abbruch, bitte geben sie naechstes mal einen Buchstaben zwischen a-l an." << endl;
-	return;
+	    cout << "Ungueltige Eingabe --> Abbruch, bitte geben sie naechstes mal einen Buchstaben zwischen a-l an." << endl;
+	    return;
     
     }
 
 }
 
 bool compute_measures() {
+
+    if(num_wohnung <= 0){
+        cout << "FEHLER: Die Aktion kann nicht durchgefuehrt werden\nSie haben keine Wohnungen angelegt. Fuegen Sie mit der jeweiligen Option mindestens eine Wohnung hinzu" << endl;
+        return false;
+    }
 
     wohnflaeche = 0;
     personen_gesamt = 0;
@@ -548,24 +704,35 @@ bool compute_measures() {
     if(wohnflaeche < 1) {
         cout << "FEHLER: Die Aktion kann nicht durchgefuehrt werden\nSie haben falsche Angben zur Flaeche der Wohnungen in m2 gemacht. Diese müssen zusammen mindestens = 1 ergeben !" << endl;
         cout << "Loeschen Sie alle Wohnungen und legen Sie alles neu an oder bearbeiten Sie eine bestimmte Wohnung um die Flaeche zu aendern." << endl;
-	return false;
-	    }
+	    return false;
+	                    }
 	
     if(personen_gesamt < 1) {
         cout << "FEHLER: Die Aktion kann nicht durchgefuehrt werden\nSie haben keiner Wohnung Personen hinzugefuegt. Loeschen Sie alle Wohnungen und legen Sie alles neu an oder bearbeiten Sie eine bestimmte Wohnung um eine Personenzahl von mindestens 1 anzugeben" << endl;
-	return false;    
-    }
+	    return false;    
+                            }
 	
-    if(heizung_gesamt) { }
+    if(heizung_gesamt < 1) { 
+        cout << "FEHLER: Die Aktion kann nicht durchgefuehrt werden\nSie haben keiner Wohnung Heizungszaehler hinzugefuegt. Loeschen Sie alle Wohnungen und legen Sie alles neu an oder bearbeiten Sie eine bestimmte Wohnung um einen korrekten Heizungswert anzugeben" << endl;
+        return false;
+                           }
 
 
     //allgemeine Stromkosten
     allgemeine_stromkosten = allgemeiner_stromverbrauch * strompreis;
 
     //wasserpreis
+    if(wasser_gesamtverbrauch < 1) {
+        cout << "FEHLER: Die Aktion kann nicht durchgefuehrt werden\nSie haben keinen Wert groesser 1 für den Wasser Gesamtverbrauch angegeben. Aendern Sie die Kennzahl mit der jeweiligen Option" << endl;
+        return false;
+    }
     wasserpreis = wasser_gesamtkosten / wasser_gesamtverbrauch;
 
     //gaspreis
+    if(gas_gesamtverbrauch < 1) {
+        cout << "FEHLER: Die Aktion kann nicht durchgefuehrt werden\nSie haben keinen Wert groesser 1 für den Gas Gesamtverbrauch angegeben. Aendern Sie die Kennzahl mit der jeweiligen Option" << endl;
+        return false;
+    }
     gaspreis = gas_gesamtkosten / gas_gesamtverbrauch;
 	
     gaseinheitspreis = gas_gesamtkosten / heizung_gesamt;
@@ -810,45 +977,64 @@ bool main_dialog() {
     cin >> option;
 
     switch (option) {
+
     case 'a':
-	cout << "\nNEUE WOHNUNG ANLEGEN , Daten werden abgefragt... " << endl;
+	    cout << "\nNEUE WOHNUNG ANLEGEN , Daten werden abgefragt... " << endl;
         wohnungsdaten_abfrage();
         return true;
-        //break;
+
     case 'b':
         cout << "\nSpeichere Eingabedaten in Datei..." << endl;
-        save_data("abrechnungsdaten.sav");
+        if (compute_measures()) { save_data("abrechnungsdaten.sav"); }        
         return true;
-        //break;
+        
     case 'c':
-        // prüfe erst ob personen und wohnungen existieren !
-        hausabrechnung();
+        if (compute_measures()) { hausabrechnung(); }
         return true;
-        //break;
+        
     case 'd':
-        // option d
+        print_general_values();
         return true;
-        //break;
+        
     case 'e':
-        // option e
+        if(num_wohnung >= 1) { wohnungen_anzeigen(); }
+        else {cout << "Es sind keine Wohnungen eingegeben oder gespeichert. Legen Sie zuerst Wohnungen mit der jeweiligen Option an." << endl; }
         return true;
-        //break;
+        
     case 'f':
-        // option c
+        kennzahl_abfrage();
         return true;
-        //break;
+        
     case 'g':
-        // option g
+        change_single_value();
         return true;
-        //break;
+        
     case 'h':
-        // option h
+        cout << "Alle Wohnungen wurden geloescht und muessen neu eingegeben werden" << endl;
+        num_wohnung = 0;
+        wohnung = {}; // name
+        quadratmeter_wohnung = {}; // qm
+        personen_wohnung = {}; // pn
+        wasserzaehler = {}; // wr
+        stromzaehler= {}; // st
+        heizungszaehler = {}; 
         return true;
-        //break;
+        
     case 'i':
-        // option i
+        cout << "Verfuegbare Wohnungen:\n" << endl;
+        wohnungen_anzeigen();
+        unsigned int ID;
+        string ID_str;        
+        try{
+        cout << "\nGeben Sie die jeweilige ID der Wohnung ein, dessen Werte veraendert werden sollen: "
+        cin >> ID_str;
+        ID = std::stoul(ID_str); 
+        
+        }
+        catch (const std::invalid_argument& e) { cout << "Der eingegebene Wert ist keine gueltige ID als ganze Zahl. Fuehren Sie die Aktion mit korrekter Eingabe erneut durch !" << endl; }
+        wohnung_aendern(ID);
         return true;
-        //break;
+
     case 'j':
         // ENDE
         return false;
@@ -905,18 +1091,28 @@ int main(int argc, char* argv[]) {
                                             }
             else { cout << "FEHLER: Die Speicherdatei ist nicht korrekt formatiert, beschaedigt oder manipuliert.\nAlle Daten muessen leider erneut eingegeben werden" << endl;
 	           kennzahl_abfrage();
-                   print_general_values();
+               print_general_values();
 	           cout << "Als naechstes muessen sie mindestens eine Wohnung anlegen, bitte geben Sie die gewuenschten Daten ein >>" << endl;
-		   wohnungsdaten_abfrage();
-                   cout << "Fuegen sie als naechstes mit der jeweiligen Option weitere Wohnungen hinzu !" << endl;		 
+		       
+               char nxt = 'n';
+               do{
+                  wohnungsdaten_abfrage(); 
+                  cout << "WEITERE WOHNUNG EINGEBN ? (y/n): "; 
+                  cin >> nxt;
+                 } while (nxt == 'y')
+
 		 }
                                          }
         else { cout << "FEHLER: Die Speicherdatei ist nicht korrekt formatiert, beschaedigt oder manipuliert.\nAlle Daten muessen leider erneut eingegeben werden" << endl;
-	       kennzahl_abfrage();
+	           kennzahl_abfrage();
                print_general_values();
-	       cout << "Als naechstes muessen sie mindestens eine Wohnung anlegen, bitte geben Sie die gewuenschten Daten ein >>" << endl;
-	       wohnungsdaten_abfrage();
-               cout << "Fuegen sie als naechstes mit der jeweiligen Option weitere Wohnungen hinzu !" << endl;
+	           cout << "Als naechstes muessen sie mindestens eine Wohnung anlegen, bitte geben Sie die gewuenschten Daten ein >>" << endl;
+               char nxt = 'n';
+               do{
+                  wohnungsdaten_abfrage(); 
+                  cout << "WEITERE WOHNUNG EINGEBN ? (y/n): "; 
+                  cin >> nxt;
+                 } while (nxt == 'y')
 	     }
 
         ifile.close();
@@ -924,16 +1120,19 @@ int main(int argc, char* argv[]) {
                 }
 
     else {
-
         
-        cout << "Speicherdatei nicht gefunden oder umbennant, Start mit Abfrage der Abrechnungsdaten... \n";
-        kennzahl_abfrage();
-        print_general_values();
-	cout << "Als naechstes muessen sie mindestens eine Wohnung anlegen, bitte geben Sie die gewuenschten Daten ein >>" << endl;
-	wohnungsdaten_abfrage();
-        cout << "Fuegen sie als naechstes mit der jeweiligen Option weitere Wohnungen hinzu !";
+               cout << "Speicherdatei nicht gefunden oder umbennant, Start mit Abfrage der Abrechnungsdaten... \n";
+               kennzahl_abfrage();
+               print_general_values();
+	           cout << "Als naechstes muessen sie mindestens eine Wohnung anlegen, bitte geben Sie die gewuenschten Daten ein >>" << endl;
+               char nxt = 'n';
+               do{
+                  wohnungsdaten_abfrage(); 
+                  cout << "WEITERE WOHNUNG EINGEBN ? (y/n): "; 
+                  cin >> nxt;
+                 } while (nxt == 'y')
 
-       }
+        }
 
     ifile.close();
 	
@@ -947,6 +1146,7 @@ int main(int argc, char* argv[]) {
     cin >> save_dialog;
     if (save_dialog == 'y') { save_values(); } */ 
 
-    cin.get();
+    //cin.get();
+    return 0;
 
 }
